53bf41703122435e301bb5d7d814a6d0a39a5c6a
Sergei Fedotov <fsa@tavda.info>
2022-12-19 23:55:24 +0700

    Доработка декодирования поля descr

--- Booking/Description.php
+++ Booking/Description.php
@@ -1,12 +1,13 @@
 <?php
 namespace Booking;
 
+use Exception;
+
 class Description
 {
     private $text;
     private $result;
     public static $error_format=0;
-    public static $param_names=[];
 
     public function __construct($text)
     {
@@ -21,17 +22,53 @@ class Description
 
     private function parseText()
     {
-        $result = ['debug' => $this->text];
+        $result = ['error'=>[]];
         $parts = explode("\n", $this->text);
         foreach ($parts as $part) {
             $p = explode(':', $part, 2);
-            if (sizeof($p)==2) {
+            if (count($p)==2) {
                 $name = trim($p[0]);
                 $value = trim($p[1]);
-                self::$param_names[$name] = true;
+                switch ($name) {
+                    case "Имя":
+                        $result['name'] = $value;
+                        break;
+                    case "Телефон":
+                        $result['phone'] = $value;
+                        break;
+                    case "Взрослых":
+                        $result['adults'] = $value;
+                        break;
+                    case "Детей":
+                        $result['kids'] = $value;
+                        break;
+                    case "Людей":
+                        $result['body'] = $value;
+                        break;
+                    case "Гости":
+                        $result['guests'] = $value;
+                        break;
+                    case "Цена":
+                        $result['price'] = $value;
+                        break;
+                    case "Оплата":
+                        $result['paid'] = $value;
+                        break;
+                    case "Предоплата":
+                        $result['prepaid'] = $value;
+                        break;
+                    case "Транспорт":
+                        $result['transport'] = $value;
+                        break;
+                    case "E-mail":
+                        $result['email'] = $value;
+                        break;
+                    default:
+                        $result['error'][] = $part;
+                }
             }
             else {
-                self::$error_format++;
+                $result['error'][] = $part;
             }
         }
         $this->result=$result;
--- index.php
+++ index.php
@@ -5,10 +5,10 @@ require_once 'Booking/Description.php';
 $pdo = new PDO(...$pdo_params);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$stmt=$pdo->query('SELECT * FROM booking WHERE id=11105');
+$stmt=$pdo->query('SELECT * FROM booking');
 while ($row=$stmt->fetchObject()) {
+    echo $row->id.PHP_EOL;
     $descr=(new Booking\Description($row->descr))->get();
-    //var_dump($descr);
+    var_dump($descr);
 }
 echo 'Всего строк с другим форматом: '. Booking\Description::$error_format . PHP_EOL;
-var_dump(array_keys(Booking\Description::$param_names));
